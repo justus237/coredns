9f5e7933149bcd08d648001277a0e92029b95b8c
Semen Bochkarev <s.bochkarev@adguard.com>
2021-01-18 19:06:54 +0300

    Pull request: DNSCrypt server refactoring.
    Squashed commit of the following:
    
    commit bddaee891e822869ee8387dc6dc69268394372c6
    Author: semen bochkarev <s.bochkarev@adguard.com>
    Date:   Mon Jan 18 17:58:41 2021 +0300
    
        fix comments
    
    commit a4c2123a89d31425143714faa5dd28609f2c4be1
    Author: semen bochkarev <s.bochkarev@adguard.com>
    Date:   Mon Jan 18 17:24:53 2021 +0300
    
        fix comments
    
    commit 2070ac6ebc15e99c5980b04f01adb032d1e5d071
    Author: semen bochkarev <s.bochkarev@adguard.com>
    Date:   Mon Jan 18 15:46:32 2021 +0300
    
        fix comments
    
    commit ea8863d08c0ebbdcf37af0e9d47fc54a720e59c8
    Author: semen bochkarev <s.bochkarev@adguard.com>
    Date:   Mon Jan 18 15:40:41 2021 +0300
    
        fixes
    
    commit 85e1a2cd9e06c618586ea5fafa916ca28f71932d
    Author: semen bochkarev <s.bochkarev@adguard.com>
    Date:   Mon Jan 18 12:18:45 2021 +0300
    
        rm addr
    
    commit 95e1a3bc4d929358689ab13607590a03b4dc747c
    Author: semen bochkarev <s.bochkarev@adguard.com>
    Date:   Fri Jan 15 19:16:21 2021 +0300
    
        upd server_dnscrypt
    
    commit cf55bf416226814291843688d483310eb422f093
    Merge: 8ea6ad3ea a2bee5822
    Author: semen bochkarev <s.bochkarev@adguard.com>
    Date:   Fri Jan 15 18:10:59 2021 +0300
    
        Merge master into 3979-3 and fix conflict
    
    commit 8ea6ad3ea3260593bcb472a19f4160f6c752b04b
    Author: semen bochkarev <s.bochkarev@adguard.com>
    Date:   Fri Jan 15 17:36:51 2021 +0300
    
        modify dnscrypt server
    
    commit d3425ed34b7da069f5c82c05f19277977ce6d8ac
    Author: semen bochkarev <s.bochkarev@adguard.com>
    Date:   Wed Jan 13 17:38:43 2021 +0300
    
        fix readme; fix cond;
    
    commit 6b787d6dfde911a5c329fe95f57b625539760250
    Author: semen bochkarev <s.bochkarev@adguard.com>
    Date:   Wed Jan 13 17:02:11 2021 +0300
    
        fix error style
    
        add cfg priority info to readme
    
        refactor dnscrypt plugin
    
    commit e458fc245f7cddc1c0a6eaae13d4fa9e84293257
    Author: semen bochkarev <s.bochkarev@adguard.com>
    Date:   Wed Jan 13 14:41:04 2021 +0300
    
        added dnscrypt.ResolverConfig struct into dnsserrver config
    
    commit ca0bef18d86cae06d752060e44e6f51385691350
    Author: semen bochkarev <s.bochkarev@adguard.com>
    Date:   Tue Jan 12 17:31:39 2021 +0300
    
        added configuration method with {
        ..yaml..
        } block

--- core/dnsserver/server_dnscrypt.go
+++ core/dnsserver/server_dnscrypt.go
@@ -1,21 +1,44 @@
 package dnsserver
 
 import (
+	"context"
 	"errors"
 	"fmt"
 	"net"
-	"sync"
 
 	"github.com/ameshkov/dnscrypt/v2"
+	"github.com/coredns/coredns/plugin/pkg/nonwriter"
 	"github.com/coredns/coredns/plugin/pkg/reuseport"
 	"github.com/coredns/coredns/plugin/pkg/transport"
+	"github.com/miekg/dns"
 )
 
+// ServerDNSCrypt implement a server for processing requests using DNSCrypt protocol.
 type ServerDNSCrypt struct {
 	*Server
-	UDPConns       []*net.UDPConn
-	udpWG          *sync.WaitGroup
 	dnsCryptServer *dnscrypt.Server
+}
+
+// dnsCryptHandler implement dnscrypt.Handler
+type dnsCryptHandler struct {
+	server *Server
+}
+
+func (h *dnsCryptHandler) ServeDNS(rw dnscrypt.ResponseWriter, req *dns.Msg) error {
+
+	// Consider renaming DoHWriter or creating a new struct for DNSCrypt.
+	nonw := &DoHWriter{nonwriter.Writer{}, rw.LocalAddr(), rw.RemoteAddr()}
+
+	// We just call the normal chain handler - all error handling is done there.
+	// We should expect a packet to be returned that we can send to the client.
+	ctx := context.WithValue(context.Background(), Key{}, h.server)
+	h.server.ServeDNS(ctx, nonw, req)
+
+	msg := nonw.Msg
+	if msg == nil {
+		return fmt.Errorf("dw.Msg is empty")
+	}
+	return rw.WriteMsg(msg)
 }
 
 var (
@@ -30,6 +53,8 @@ func NewServerDNSCrypt(addr string, group []*Config) (*ServerDNSCrypt, error) {
 		return nil, err
 	}
 
+	// resolverConfig taken from dnsserver.Config.DNSCryptConfig.
+	// Is populated by the dnscrypt plugin from yaml file or Corefile.
 	var resolverConfig *dnscrypt.ResolverConfig
 	for _, conf := range s.zones {
 		resolverConfig = conf.DNSCryptConfig
@@ -51,8 +76,24 @@ func NewServerDNSCrypt(addr string, group []*Config) (*ServerDNSCrypt, error) {
 		dnsCryptServer: &dnscrypt.Server{
 			ProviderName: resolverConfig.ProviderName,
 			ResolverCert: cert,
+			Handler: &dnsCryptHandler{
+				server: s,
+			},
 		},
 	}, nil
+}
+
+// OnStartupComplete lists the sites served by this server
+// and any relevant information, assuming Quiet is false.
+func (s *ServerDNSCrypt) OnStartupComplete() {
+	if Quiet {
+		return
+	}
+
+	out := startUpZones(transport.DNSCrypt+"://", s.Addr, s.zones)
+	if out != "" {
+		fmt.Print(out)
+	}
 }
 
 func (s ServerDNSCrypt) Listen() (net.Listener, error) {
@@ -84,11 +125,9 @@ func (s ServerDNSCrypt) ListenPacket() (net.PacketConn, error) {
 
 func (s ServerDNSCrypt) ServePacket(p net.PacketConn) error {
 	if UDPConn, ok := p.(*net.UDPConn); ok {
-		for {
-			err := s.dnsCryptServer.ServeUDP(UDPConn)
-			if err != nil {
-				return err
-			}
+		err := s.dnsCryptServer.ServeUDP(UDPConn)
+		if err != nil {
+			return err
 		}
 	}
 	return fmt.Errorf("fail conversion %T to *net.UDPConn", p)
